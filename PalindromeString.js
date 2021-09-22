aea17527ab0b8c52161c7dad0af489ab62f281bd
Askar07 <askar.allah@anywhere.co>
2021-09-22 20:17:13 +0530

    Update PalindromeString.js

--- PalindromeString.js
+++ PalindromeString.js
@@ -1,11 +1,13 @@
-function checkPalindrome() {
-    var stringVal = document.getElementById("stringVal").value
+function checkPalindrome(stringVal) {
+    //var stringVal = document.getElementById("stringVal").value
     var arrValues = stringVal.split('')
     var revArrValues = arrValues.reverse()
     var revString = revArrValues.join('')
 
     if (stringVal == revString){
-        document.getElementById("p1").innerHTML = stringVal+" "+ "is a Palindrome"
-    }else document.getElementById("p1").innerHTML = stringVal+" "+ "is not a Palindrome" 
+        /*document.getElementById("p1").innerHTML =*/ console.log(stringVal+" "+ "is a Palindrome")
+    }else /*document.getElementById("p1").innerHTML = */console.log(stringVal+" "+ "is not a Palindrome") 
     
 }
+var stringVal = prompt("Enter a string:")
+checkPalindrome(stringVal)
