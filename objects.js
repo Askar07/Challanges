7d92c70d413939a515a3ca9fdf58c63d364f1de3
Askar07 <askar.allah@anywhere.co>
2021-09-23 13:06:53 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -3,28 +3,36 @@ const person = {
     personName:"Askar",
     age:20,
     marks:[
-        m1=95,
-        m2=85,
-        m3=100,
-        m4=50,
-        m5=80
+        m1 = 95,
+        m2 = 85,
+        m3 = 100,
+        m4 = 50,
+        m5 = 90
     ]
 }
-var result = person.marks.forEach(check)
-function check(result) {
-    if (result > 35 ){
-        return pass() 
-        stop;
-    }else return fail()
-}
+var result = person.marks.every(check)
+function check(value) {
+        return value > 35        
+} if (result == true){
+    display()
+ }else fail()
 
-//function totalMarksAndPercentage() {
-  //  var totalMarks = person.marks.reduce((a,b) => a+b,0)
-    //var percentage = ''
-//}
-function pass(){
+var total = person.marks.reduce(getReport)
+function getReport(sum,val) {
+    return sum + val
+} var percent = (total/500)*100
+function display(){
+    pass()
+    console.log("ID:",person.id,"\n","Name:",person.personName,"\n","Age:"+ person.age,"\n","Marks:"+person.marks) 
+
+}
+function pass() {
     console.log("Passed")
 }
 function fail(){
-    console.log("Failed")
+    console.log("Failed") 
+    console.log("ID:",person.id,"\n","Name:",person.personName,"\n","Age:"+ person.age,"\n","Marks:"+person.marks)
+     
 }
+console.log("Total Marks:",total)
+console.log("Percentage:",percent)
